f4c19617de3e66f66d442ef71148f95371e512a4
Pawel Dabala <pdabala@gmail.com>
2017-09-29 22:32:42 +0200

    10.2 complete

---
+++ Question10_2.py
@@ -0,0 +1,20 @@
+"""
+Question 10
+Level 2
+
+Question:
+Write a program that accepts a sequence of whitespace separated words as input and prints the words
+after removing all duplicate words and sorting them alphanumerically.
+Suppose the following input is supplied to the program:
+hello world and practice makes perfect and hello world again
+Then, the output should be:
+again and hello makes perfect practice world
+"""
+def remve_duplicate(text):
+
+    li =text.split()
+
+    return " ".join(sorted(list(set(li))))
+
+
+print(remve_duplicate("hello world and practice makes perfect and hello world again"))
